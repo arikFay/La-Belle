8c5624dc2f7888867719562bf772ea5ebaffd0cd
arik <arikbenfay@gmail.com>
2024-02-06 21:36:41 +0200

    menu

--- navigations/CustomDrawer.js
+++ navigations/CustomDrawer.js
@@ -1,46 +1,32 @@
-import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import React from 'react';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { Color, FontFamily, FontSize } from '../GlobalStyles';
 
+
 import Svg, { Path, Defs, Pattern, Use } from "react-native-svg"
-const SvgComponent = (props) => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={83}
-    height={83}
-    fill="none"
-    {...props}
-  >
-    <Path
-      fill="url(#a)"
-      stroke="#fff"
-      d="M-5.181-2.739h94.338v94.338H-5.181z"
-    />
-    <Defs>
-      <Pattern
-        id="a"
-        width={1}
-        height={1}
-        patternContentUnits="objectBoundingBox"
-      >
-        <Use transform="scale(.00195)" />
-      </Pattern>
-    </Defs>
-  </Svg>
-)
+
+const { width } = Dimensions.get('screen')
 
 const CustomDrawer = props => {
-  return (
-    <DrawerContentScrollView style={styles.drawerStyle}>
-        <View>
-            <SvgComponent />
-        </View>
-        <View>
-            <DrawerItemList {...props}/>
-        </View>
-    </DrawerContentScrollView>
-  )
+    return (
+        <DrawerContentScrollView style={styles.drawerStyle}>
+            <View style={{ height: 140, flexDirection: 'row-reverse' }}>
+                <Image
+                    style={styles.profileImage}
+                    resizeMode="cover"
+                    source={require("../assets/imageProfile.jpg")} />
+
+                <View style={styles.profileTitle}>
+                    <Text style={[styles.title, styles.textTypo]}>נאווה ירושלמי</Text>
+                    <Text style={[styles.subTitle, styles.textTypo]}>מודל היופי בע”מ</Text>
+                    </View>
+            </View>
+            <View>
+                <DrawerItemList {...props} />
+            </View>
+        </DrawerContentScrollView>
+    )
 }
 
 export default CustomDrawer;
@@ -54,10 +40,29 @@ const styles = StyleSheet.create({
         overflow: "hidden",
     },
     profileImage: {
-        // top: 35,
-        // left: 324,
-        // height: 85,
-        // width: 85,
+        left: "10%",
+        height: 85,
+        width: 85,
+        borderRadius: 85 / 2,
+        marginLeft: "10%"
         // position: "absolute",
+    },
+    profileTitle: {
+        top: 85 / 4,
+    },
+    textTypo: {
+      color: Color.colorWhite,
+      fontFamily: 'OpenSansHebrew-Regular',
+      fontWeight: "700",
+    },
+    title: {
+        fontSize: 22,
+    },
+    subTitle: {
+        fontSize: 16,
+        direction: 'rtl',
+        fontFamily: FontFamily.openSansHebrew,
+        textAlign: "left",
+
     }
 })
